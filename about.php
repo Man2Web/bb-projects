7031ee20ba18feb4ce9e28e7225231f047bc2b83
Manikandan <mantwoweb@gmail.com>
2022-11-26 11:07:59 +0530

    about content change

--- about.php
+++ about.php
@@ -37,9 +37,9 @@
                             We seek to promote, execute, and deliver residential and commercial builds of enduring quality and sustainable resource-use,
                             that meet our customers’ needs, with transparency, trustworthiness and profitability.</p>
                         </div>
-                        <div class="about-us-info-wrap-inner about-us-info-devide---">
+                        <!-- <div class="about-us-info-wrap-inner about-us-info-devide---">
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
